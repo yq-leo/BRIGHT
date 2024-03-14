5ee250b03b32e6906738eebd01a6cd0e46a6bab3
Leo Yu <qiyuleo@outlook.com>
2024-03-13 19:58:14 -0500

    upload cora results

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
-data = "DBLP"
+data = "Cora"
 dim = 128
 norm_g1_file = "Data/" + data + "/norm_data/network1.tsv"
 norm_g2_file = "Data/" + data + "/norm_data/network2.tsv"
@@ -12,5 +12,5 @@ rwr2_emd = "Data/" + data + "/split/rwr_emd2_"
 gcn_data = "Data/" + data + "/norm_data/gcn_data.pkl"
 g1_geo_data = "Data/" + data + "/norm_data/g1_geo.pkl"
 g2_geo_data = "Data/" + data + "/norm_data/g2_geo.pkl"
-numpy_file = 'Data/DBLP/ori_data/new-ACM-DBLP'
-# numpy_file = 'Data/Cora/ori_data/noisy-cora1-cora2'
+# numpy_file = 'Data/DBLP/ori_data/new-ACM-DBLP'
+numpy_file = 'Data/Cora/ori_data/noisy-cora1-cora2'
